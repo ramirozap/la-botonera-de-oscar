79bdd6cd7542aad4686bbfdff675b1bbf7503c6a
Ramiro Zapata <ramiro.zapata@weareadaptive.com>
2020-11-10 21:42:39 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const audioFiles: { [key: string]: { path: string; color: string } } = {
+    "veni oscar": {
+      color: "#f6dbba",
+      path: "audio/veni-oscar.mp3",
+    },
+    "veni amigo": {
+      color: "#db604c",
+      path: "audio/veni-amigo.mp3",
+    },
+    hola: {
+      color: "#b13353",
+      path: "audio/hoooola.mp3",
+    },
+    "que te has puesto": {
+      color: "#5e2052",
+      path: "audio/que-te-has-puesto.mp3",
+    },
+    "andas con frio": {
+      color: "#74c99e",
+      path: "audio/andas-con-frio.mp3",
+    },
+    "a que vieja se lo robaste": {
+      color: "#317c87",
+      path: "audio/a-que-vieja-se-lo-robaste.mp3",
+    },
+    "pero que lindo te queda": {
+      color: "#271854",
+      path: "audio/pero-que-lindo-te-queda.mp3",
+    },
+    "pero que linda bufanda": {
+      color: "#1a1016",
+      path: "audio/pero-que-linda-bufanda.mp3",
+    },
+  };
+
+  const play = (path: string) => {
+    const audio = new Audio(path);
+    audio.play();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {Object.keys(audioFiles).map((key) => (
+        <button
+          onClick={() => play(audioFiles[key].path)}
+          style={{ background: audioFiles[key].color }}
         >
-          Learn React
-        </a>
-      </header>
+          {key}
+        </button>
+      ))}
     </div>
   );
 }
